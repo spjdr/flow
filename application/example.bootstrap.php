819934a10c62f33001fe22a026450cbe3947d451
spjdr <softshare@spjdr.dk>
2011-04-12 19:37:00 +0200

    alpha version - including bootstrap and db

--- application/example.bootstrap.php
+++ application/example.bootstrap.php
@@ -58,7 +58,7 @@ if (getenv('KOHANA_ENV') !== FALSE)
  * - boolean  caching     enable or disable internal caching                 FALSE
  */
 Kohana::init(array(
-	'base_url'   => 'http://spjdr.dk/flow',
+	'base_url'   => 'http://localhost:8888/spjdr/flow/',
 	'index_file' => '',
 	'profiling' => ! IN_PRODUCTION,
 	'caching' => IN_PRODUCTION
@@ -78,16 +78,18 @@ Kohana::$config->attach(new Kohana_Config_File);
  * Enable modules. Modules are referenced by a relative or absolute path.
  */
 Kohana::modules(array(
-	 'auth'       => MODPATH.'auth',       // Basic authentication
-	 'cache'      => MODPATH.'cache',      // Caching with multiple backends
-	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
-	 'database'   => MODPATH.'database',   // Database access
-	 'image'      => MODPATH.'image',      // Image manipulation
-	 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
-	// 'oauth'      => MODPATH.'oauth',      // OAuth authentication
-	 'pagination' => MODPATH.'pagination', // Paging of results
-	// 'unittest'   => MODPATH.'unittest',   // Unit testing
-	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
+	 'auth'       => MODPATH.'auth',		// Basic authentication
+	 'cache'      => MODPATH.'cache',		// Caching with multiple backends
+	 'fust'		  => MODPATH.'fust',		
+	// 'codebench'  => MODPATH.'codebench',	// Benchmarking tool
+	 'database'   => MODPATH.'database',	// Database access
+	 'image'      => MODPATH.'image',		// Image manipulation
+	 'orm'        => MODPATH.'orm',			// Object Relationship Mapping
+	// 'oauth'      => MODPATH.'oauth',		// OAuth authentication
+	 'pagination' => MODPATH.'pagination',	// Paging of results
+	 'email'		  => MODPATH.'banks-kohana-email',
+	// 'unittest'   => MODPATH.'unittest',	// Unit testing
+	// 'userguide'  => MODPATH.'userguide',	// User guide and API documentation
 	));
 
 /**
@@ -95,51 +97,131 @@ Kohana::modules(array(
  * defaults for the URI.
  */
 
-Route::set('create','create')
+Route::set('noaccess','noaccess(.<format>)')
+	->defaults(array(
+		'controller'=>'pages',
+		'action'=>'view',
+		'page'=>'noaccess',
+		'format'=>'html'
+	));
+
+Route::set('pages','pages/<page>(/<action>(.<format>))')
+	->defaults(array(
+		'controller'=>'pages',
+		'action'=>'view',
+		'format'=>'html'
+	));
+
+Route::set('flow-new','new(.<format>)')
 	->defaults(array(
 		'controller'=>'flows',
-		'action'=>'create'
+		'action'=>'new',
+		'format'=>'html'
 	));
 	
-Route::set('index','index(@<username>)')
+Route::set('join','join/<secret>')
 	->defaults(array(
-		'controller'=>'flows',
-		'action'=>'index'
+		'controller'=>'join',
+		'action'=>'accept'
 	));
 	
-Route::set('account','<action>',
+Route::set('account-actions','<action>(.<format>)',
 	array(
 		'action'=>'(login|logout|register)'
 	))
 	->defaults(array(
-		'controller'=>'account'
+		'controller'=>'account',
+		'format'=>'html'
 	));
 
-Route::set('my_account','@<username>(/<action>)',
+Route::set('my-account','@<username>(/<action>)(.<format>)',
 	array('username'=>'(.*?)')
 	)
 	->defaults(array(
 		'controller'	=>'account',
-		'action'		=>'view'
+		'action'		=>'view',
+		'format'=>'html'
 	)); 
 
-Route::set('edit','<uri>/<action>',array('action'=>'edit|delete'))
+Route::set('flow-actions','<flow>(/<action>)(.<format>)',array('action'=>'edit|delete|view'))
 	->defaults(array(
 		'controller'=>'flows',
-		'action'=>'edit'
-	));
-	
-Route::set('tags','<uri>/tags(/<id>/<action>)')
-	->defaults(array(
-		'controller'=>'tags',
-		'action'=>'index'
+		'action'=>'view',
+		'format'=>'html'
 	));
 
-Route::set('default','<uri>(/<controller>(/<action>))')
+Route::set('flow-items','<flow>/<controller>(/<action>)(.<format>)',array('controller'=>'tags|invitations|users','action'=>'index|new'))
+	->defaults(array(
+		'controller'=>'tags',
+		'action'=>'index',
+		'format'=>'html'
+	));
+
+Route::set('flow-item','<flow>/<controller>/<item>(/<action>)(.<format>)',array('controller'=>'tags|invitations'))
+	->defaults(array(
+		'controller'=>'tags',
+		'action'=>'view',
+		'format'=>'html'
+	));
+
+Route::set('flow-user','<flow>/@<username>(/<action>)(.<format>)')
+	->defaults(array(
+		'controller'=>'users',
+		'action'=>'view',
+		'format'=>'html'
+	));
+
+Route::set('stream-new','<flow>/new(.<format>)')
+	->defaults(array(
+		'controller'=>'streams',
+		'action'=>'new',
+		'format'=>'html'
+	));
+	
+Route::set('stream-index','<flow>/streams(.<format>)')
+	->defaults(array(
+		'controller'=>'streams',
+		'action'=>'index',
+		'format'=>'html'
+	));
+
+Route::set('event-new','<flow>/<stream>/new(.<format>)')
+	->defaults(array(
+		'controller'=>'events',
+		'action'=>'new',
+		'format'=>'html'
+	));
+
+Route::set('stream','<flow>/<stream>(/<action>)(.<format>)',array('action'=>'edit|delete|view'))
+	->defaults(array(
+		'controller'=>'streams',
+		'action'=>'view',
+		'format'=>'html'
+	));
+
+Route::set('stream-appoint','<flow>/<stream>/<editee>/@<username>(.<format>)',array('editee'=>'withdraw|appoint'))
+	->defaults(array(
+		'controller'=>'users',
+		'action'=>'view',
+		'format'=>'html'
+	));
+
+Route::set('event','<flow>/<stream>/<event>(/<action>)',array('action'=>'edit|delete|view|lock'))
+	->defaults(array(
+		'controller'=>'events',
+		'action'=>'view',
+		'format'=>'html'
+	));
+
+/*
+Route::set('default','<flow>(/<controller>(/<action>))',
+	array('controller'=>'flows|tags|invitations')
+	array('action'=>'new')
 	->defaults(array(
 		'controller'=>'flows',
 		'action'=>'view'
 	));
+*/
 
 Route::set('home', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
@@ -165,7 +247,6 @@ catch (Exception $e)
         // If there was an error, send a 404 response and display an error
         $request->status   = 404;
         $request->response = View::factory('errors/404');
-
 }
 
 if ( $request->send_headers()->response )
