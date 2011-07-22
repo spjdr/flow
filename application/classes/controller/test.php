62e969d1d13a7710f902c457b5e61743f656dda4
spjdr <softshare@spjdr.dk>
2011-07-22 11:32:55 +0200

    Simoulatenous events and xml export.

--- application/classes/controller/test.php
+++ application/classes/controller/test.php
@@ -1,8 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Welcome extends Controller {
+class Controller_Test extends Controller {
 
-	public function action_view()
+	public function action_index()
 	{
 		/*foreach (ORM::factory('event')->find_all() as $event){
 			$event->save();
@@ -14,6 +14,7 @@ class Controller_Welcome extends Controller {
 		foreach(ORM::factory('flow')->find_all() as $flow) {
 			$flow->save();
 		}
+		$this->request->response = 'hello, world!';
 	}
 
 } // End Welcome
